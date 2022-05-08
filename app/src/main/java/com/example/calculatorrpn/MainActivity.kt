ee2deeb5d3ddf7b05fa26f5da8c8b68693796d86
PawelDopierala <pawel.dopierala4@gmail.com>
2022-05-08 17:17:25 +0200

    Initial commit

---
+++ app/src/main/java/com/example/calculatorrpn/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.calculatorrpn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.LinearLayout
+import android.widget.TableLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
