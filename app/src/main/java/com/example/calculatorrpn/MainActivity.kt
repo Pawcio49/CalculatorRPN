317a46d7c9e0959507b84fed4760e8e66ca924b9
PawelDopierala <pawel.dopierala4@gmail.com>
2022-05-09 12:40:43 +0200

    finish front

--- app/src/main/java/com/example/calculatorrpn/MainActivity.kt
+++ app/src/main/java/com/example/calculatorrpn/MainActivity.kt
@@ -8,8 +8,13 @@ import android.widget.LinearLayout
 import android.widget.TableLayout
 
 class MainActivity : AppCompatActivity() {
+    val stack = mutableListOf<Double>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        stack.add(1.0)
+        println(stack)
     }
 }
